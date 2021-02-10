c2a3678f6ac30a46a08e931a380a37a2442b2fc6
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-10 03:23:25 -0300

    Javascript funcionando, modificações são vistas e codigo aparece no textarea

--- 002-border-radius-preview/script.js
+++ 002-border-radius-preview/script.js
@@ -1,11 +1,26 @@
 var box = document.querySelector('.box');
-var values = document.querySelectorAll('.values[value]');
-for(let i = 0; i < values.length; i++){
-    values[i].addEventListener('change', function(){
-        console.log(i);
-        console.log(values[i].value);
-        if(i == 0){
-            box.style.borderRadius = `${values[i].value}px`;
+var input = document.querySelectorAll('.values');
+var result = document.querySelector('.result');
+
+for(let i = 0; i < input.length; i++){
+    input[i].addEventListener('change', function(){
+        if(!isNaN(input[i].value)){ // Verificando se o valor do input é numerico
+                if(i == 0){ //  0  == bordar superior a esquerda
+                    box.style.borderTopLeftRadius = `${input[i].value}px`;
+                    result.innerHTML += `border-top-left-radius: ${input[i].value}px;` + '\r\n'; // \r\n = quebra de linha
+                }
+                else if(i == 1){ // 1 == bordar superior a direita
+                    box.style.borderTopRightRadius= `${input[i].value}px`;
+                    result.innerHTML += `border-top-right-radius: ${input[i].value}px;` + '\r\n';
+                }
+                else if(i == 2){// 2 == bordar inferior a esquerda
+                    box.style.borderBottomLeftRadius = `${input[i].value}px`;
+                    result.innerHTML += `border-bottom-left-radius: ${input[i].value}px;` + '\r\n';
+                }
+                else{   // 3 == bordar inferior a direita
+                    box.style.borderBottomRightRadius = `${input[i].value}px`;
+                    result.innerHTML += `border-bottom-right-radius: ${input[i].value}px` + '\r\n';
+                }
         }
     })
 }

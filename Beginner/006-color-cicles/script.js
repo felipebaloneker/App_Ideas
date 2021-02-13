bcbb908c34c93468f6052d96b1e0e4c376785aed
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-13 15:35:11 -0300

    Adicionado Troca de velocidade animação

--- Beginner/006-color-cicles/script.js
+++ Beginner/006-color-cicles/script.js
@@ -1,10 +1,15 @@
 var background = document.querySelector('.background');
 var color = document.querySelectorAll('.color');
-
+var timer = document.querySelector('.speed');
 for(i = 0; i <color.length; i++){
-    color[i].addEventListener('change',troca)
+    color[i].addEventListener('change',Troca)
 }
 // trocando fundo
-function troca(){
-    background.style.backgroundColor= `rgba(${color[0].value},${color[1].value},${color[2].value},${color[3].value})`;
+function Troca(){
+        background.style.backgroundColor= `rgba(${color[0].value},${color[1].value},${color[2].value},${color[3].value})`;
 }
+
+function Speed(){
+    background.style.animationDuration = `${timer.value}s`;
+}
+timer.addEventListener('change',Speed);

651cf256b1691a9c269b9141b4579b3c65ca1294
Felipe Baloneker <55571514+felipebaloneker@users.noreply.github.com>
2021-02-13 15:55:58 -0300

    Adicionado botão Parar Animação

--- Beginner/006-color-cicles/script.js
+++ Beginner/006-color-cicles/script.js
@@ -8,8 +8,22 @@ for(i = 0; i <color.length; i++){
 function Troca(){
         background.style.backgroundColor= `rgba(${color[0].value},${color[1].value},${color[2].value},${color[3].value})`;
 }
-
+// velocidade da animação
 function Speed(){
     background.style.animationDuration = `${timer.value}s`;
 }
 timer.addEventListener('change',Speed);
+
+// parando e iniciando animação
+var btStop = document.querySelector('.btstop');
+btStop.addEventListener("click",Stop)
+function Stop(){
+    if(btStop.textContent == 'Parar'){
+        btStop.textContent = 'Iniciar';
+        background.style.animation = 'none';
+    }
+    else{
+        btStop.textContent = 'Parar'
+        background.removeAttribute('style');
+    }
+}
